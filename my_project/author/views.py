14b7710947549484b889e5e2596e2284639cfbcc
pochangl <pochangl@usc.edu>
2016-01-28 22:26:59 +0800

    url

--- my_project/author/views.py
+++ my_project/author/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
 from django.http.response import HttpResponse
+from django.core.urlresolvers import reverse
 
 # Create your views here.
 
 
 def home_view(request, *args, **kwargs):
     return HttpResponse("<html><head></head><body>I am home page</body></html>")
+
+def param(request, *args, **kwargs):
+    return HttpResponse(str(kwargs))
+
+
+def reverse_view(request, *args, **kwargs):
+
+    return HttpResponse(reverse('author:user_page', kwargs={"user_id": "99"}))
--- my_project/my_project/urls.py
+++ my_project/my_project/urls.py
@@ -13,11 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
-from author.views import home_view
+from author import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', home_view )
+    url(r'^$', views.home_view, name="home"),
+    
+    
+    url(r'^author/', include('author.urls', namespace="author")),
+    
 ]
