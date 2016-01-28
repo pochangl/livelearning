692e5460623c1ec12fd27db2b3e4ebfabe31394e
pochangl <pochangl@usc.edu>
2016-01-28 22:27:27 +0800

    增加author Model

--- my_project/author/models.py
+++ my_project/author/models.py
@@ -3,3 +3,15 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=16)
+    birth_time = models.DateTimeField(null=True, default=None)
+    birth_date = models.DateField(null=True, default=None)
+    age = models.PositiveIntegerField(default=0)
+    bio = models.TextField(blank=True, default="")
+
+
+
+
