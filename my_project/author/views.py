9bf18f7cc781622a18f0913ad27df1a1877f71b8
pochangl <pochangl@usc.edu>
2016-01-28 20:52:06 +0800

    replace home page

--- my_project/author/views.py
+++ my_project/author/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http.response import HttpResponse
 
 # Create your views here.
+
+
+def home_view(request, *args, **kwargs):
+    return HttpResponse("<html><head></head><body>I am home page</body></html>")
--- my_project/my_project/urls.py
+++ my_project/my_project/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from author.views import home_view
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', home_view )
 ]
