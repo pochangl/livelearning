6edd3ad054c5b851db56b1fc42a84c3caba193c6
pochangl <pochangl@usc.edu>
2016-02-04 22:20:54 +0800

    day 2

--- my_project/author/admin.py
+++ my_project/author/admin.py
@@ -1,6 +1,18 @@
 from django.contrib import admin
 
 from . import models
+from django.contrib.admin.options import ModelAdmin
 # Register your models here.
 
-admin.site.register(models.Author)
+class AuthorAdmin(ModelAdmin):
+    list_display = ("name", "birth_time", "birth_date", "age", "bio", "sex")
+
+class BookAdmin(ModelAdmin):
+    list_display = ("id", "author", "title")
+
+
+admin.site.register(models.Author, AuthorAdmin)
+admin.site.register(models.Book, BookAdmin)
+admin.site.register(models.OneToOneBook)
+admin.site.register(models.ManyToManyBook)
+admin.site.register(models.DetailedBook)
--- my_project/author/models.py
+++ my_project/author/models.py
@@ -4,14 +4,47 @@ from django.db import models
 
 # Create your models here.
 
+SEX_CHOICES = (
+    ("F", "Female"),
+    ("M", "Male")
+)
+
 
 class Author(models.Model):
-    name = models.CharField(max_length=16)
+    # id = models.PositiveInteger(primary_key=True)
+    name = models.CharField("author_name", max_length=16, primary_key = True)
     birth_time = models.DateTimeField(null=True, default=None)
     birth_date = models.DateField(null=True, default=None)
     age = models.PositiveIntegerField(default=0)
     bio = models.TextField(blank=True, default="")
+    sex = models.CharField(max_length=16, choices = SEX_CHOICES)
+    # books
+
+    def __str__(self):
+        return self.name
 
 
+class Book(models.Model):
+    author = models.ForeignKey(Author, related_name="books") # foreign key author references author_author(name)
+    title = models.CharField(max_length=32)
+
+    def __str__(self):
+        return self.title
+
+class OneToOneBook(models.Model):
+    author = models.OneToOneField(Author, related_name="one_to_one_book")
+
+
+class ManyToManyBook(models.Model):
+    title = models.CharField(max_length=32)
+    authors = models.ManyToManyField(Author, related_name="many_to_many_book")
+
+    def __str__(self):
+        return "M2M: %s" % self.title
+
+
+class DetailedBook(Book):
+    # models.foreignKeyField(Book)
+    year = models.PositiveIntegerField()
 
 
